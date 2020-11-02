4c6dd63eba070c94faee7ba9527c3af2da6e6d0f
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-11-02 13:03:59 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -12,3 +12,21 @@ The program should display the factored form in an Entry widget.
 Extension: make the + between a,b and b,c buttons that will toggle
 between + and -.
 """
+import tkinter as tk
+from tkinter import *
+window = tk.Tk()
+
+bv = StringVar()
+bv.set("B")
+cv = StringVar()
+cv.set("C")
+
+instuct = tk.Label(window, text="Enter b and c and the program will factor them")
+b = tk.Entry(window, textvar=bv)
+c = tk.Entry(window, textvar=cv)
+
+instuct.grid(row=1,column=1,columnspan=2)
+b.grid(row=2,column=1)
+c.grid(row=2,column=2)
+
+window.mainloop()
